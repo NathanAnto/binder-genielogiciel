9f05530aa9e14de3728f9c42be761403e73f084c
mariethoz <cedric.mariethoz.cm@gmail.com>
2025-01-05 13:37:00 +0100

    Add comment to the code

--- server/database.js
+++ server/database.js
@@ -1,13 +1,19 @@
 import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('./server/booking_app.db');
-// For online use (master);
-//const db = 'postgres://user:password@host:port/database_name';
+// Open the booking_app.db database
+const db = new sqlite3.Database('./booking_app.db');
 
-
+/**
+ * Execute a SQL query with optional parameters.
+ * @param {string} query - The SQL query to execute.
+ * @param {Array} parms - Optional parameters for the query.
+ * @returns {Promise<Array>} - A promise that resolves with the query results as an array of rows.
+ */
 export const executeQuery = (query, parms = []) => {
     return new Promise((resolve, reject) => {
+        // Run the query with the given parameters
         db.all(query, URLSearchParams, (err, rows) => {
+            // Handle errors or return the rows
             if (err) {
                 reject(err);
             } else {
--- server/initDatabase.js
+++ server/initDatabase.js
@@ -5,19 +5,26 @@ const DB_FILE = './booking_app.db';         // Path to database file
 const SCHEMA_FILE = './server/schema.sql';  // Path to schema file
 const DATA_FILE = './server/data.sql';      // Path to data file
 
+/**
+ * Initialize the database. If the database file already exists, skip initialization.
+ * If the database file does not exist, create it using the schema file and feed it data.
+ */
 const initDatabase = () => {
     let db;
 
     if (fs.existsSync(DB_FILE)) {
         console.log('Database already exists. Skipping initialization.');
-        console.log('To update the database delete it and run server/initDatabase.js');
-        return
+        console.log('To update the database, delete it and run server/initDatabase.js');
+        return;
     } else {
+        // Read the schema file
         const schema = fs.readFileSync(SCHEMA_FILE, 'utf-8');
+        // Create a new database file
         db = new sqlite3.Database(DB_FILE);
 
         db.serialize(() => {
             console.log('Creating database...');
+            // Execute the schema SQL to set up the database
             db.exec(schema, (err) => {
                 if (err) {
                     console.error('Error creating database:', err);
@@ -28,20 +35,24 @@ const initDatabase = () => {
         });
     }
 
+    // Read the data file
     const data = fs.readFileSync(DATA_FILE, 'utf-8');
 
     db.serialize(() => {
         console.log('Feeding database...');
+        // Execute the data SQL to populate the database
         db.exec(data, (err) => {
             if (err) {
                 console.error('Error feeding database:', err);
             } else {
-                console.log('Database feeded successfully.');
+                console.log('Database fed successfully.');
             }
         });
     });
 
+    // Close the database connection
     db.close();
 };
 
+// Run the initDatabase function to initialize the database
 initDatabase();
