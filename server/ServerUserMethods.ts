523d2088cbff3ee1d27754da15230a74fc909e0f
mariethoz <cedric.mariethoz.cm@gmail.com>
2025-02-01 10:29:44 +0100

    Comments and docstring for server

--- server/ServerUserMethods.ts
+++ server/ServerUserMethods.ts
@@ -4,35 +4,52 @@ import { User } from "/imports/types/user";
 import { Accounts } from "meteor/accounts-base";
 
 Meteor.methods({
+    /**
+     * Fetches all users from the database.
+     * @returns {Promise<any>} A promise that resolves to the list of all users.
+     */
     async server_getUsers(): Promise<any> {
         return await executeQuery('SELECT * FROM Users');
     },
+
+    /**
+     * Fetches a user by their email from the database.
+     * @param {string} email - The email of the user to fetch.
+     * @returns {Promise<any>} A promise that resolves to the user with the specified email.
+     */
     async server_getUserByEmail(email: string): Promise<any> {
         return await executeQuery('SELECT * FROM Users WHERE email = ?', [email]);
     },
+
+    /**
+     * Creates a new user in the database.
+     * @param {User} user - The user object containing user details.
+     * @returns {Promise<any>} A promise that resolves when the user is created.
+     * @throws {Meteor.Error} If a user with the same email already exists or if there is a server error.
+     */
     async server_createUser(user: User): Promise<any> {    
         const cleanedEmail = user.email.trim().toLowerCase();
     
-        // Utiliser findOneAsync pour rechercher l'utilisateur
+        // Use findOneAsync to search for the user
         const existingUser = await Meteor.users.findOneAsync({ 'emails.address': cleanedEmail });
         if (existingUser) {
-          console.error("Utilisateur existant trouvé :", existingUser);
-          throw new Meteor.Error('user-exists', 'Un utilisateur avec cet email existe déjà.');
+          console.error("Existing user found:", existingUser);
+          throw new Meteor.Error('user-exists', 'A user with this email already exists.');
         }
     
         try {
-          // Créer l'utilisateur
+          // Create the user
           const userId = Accounts.createUser({
             username: user.name,
             email: cleanedEmail,
             password: user.password,
           });
 
-          console.log("Utilisateur créé avec succès, ID :", userId);
+          console.log("User created successfully, ID:", userId);
           return await executeQuery('INSERT INTO Users (name, email, password) VALUES (?, ?, ?)', [user.name, user.email, user.password]);
         } catch (error) {
-          console.error("Erreur lors de la création de l'utilisateur :", error);
-          throw new Meteor.Error('server-error', 'Erreur interne lors de la création de l\'utilisateur.');
+          console.error("Error creating user:", error);
+          throw new Meteor.Error('server-error', 'Internal server error while creating the user.');
         }
     }
 });
